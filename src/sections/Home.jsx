f369be8e615e8e35c4da1d195dddc4e4a21b82e7
iddev5 <ayushbardhan5@gmail.com>
2024-12-30 11:41:58 +0530

    Add dynamic fog effect as background using vanta and threejs

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,7 +1,44 @@
+"use client";
+
+import React, { useState, useEffect, useRef } from 'react'
+import * as THREE from 'three';
+import FOG from 'vanta/dist/vanta.fog.min.js'
+
+const useVanta = (effect) => {
+  const [vantaEffect, setVantaEffect] = useState(null)
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(effect())
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
+};
+
 export const HeroSection = () => {
+  const backRef = useRef(null);
+
+  const fogEffect = useVanta(() => FOG({
+    el: backRef.current,
+    THREE: THREE,
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0xff0000,
+    midtoneColor: 0x0,
+    lowlightColor: 0x0,
+    baseColor: 0x0,
+    blurFactor: 0.81,
+    speed: 0.00,
+    zoom: 0.60
+  }));
+
   return <div className='bg-black sm:h-[80vh] md:h-screen flex flex-col items-center justify-center space-y-16 font-helvetica'>
-    <div className="h-[350vh] w-screen hero-gradient absolute left-1/2 bottom-1/3"></div>
-    <div className="h-[350vh] w-screen hero-gradient absolute right-1/2 bottom-[50%]"></div>
+    <div ref={backRef} className='absolute sm:h-[80vh] md:h-screen w-screen opacity-50'></div>
 
     <h1 className="text-white text-center font-bold sm:text-5xl md:text-7xl z-10">
       Ideas Worth Spreading
