6285a56106fa821c9944d47ca914b001d67dc067
UTKARSHA-TEWARY <utkarsha7781@gmail.com>
2025-01-05 16:09:46 +0530

    Updated Crew component with new styling and functionality, added images

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,13 @@
 
 import Crew from '../sections/Crew';
 
-import About from '../sections/About.jsx';
-import { HeroSection } from '../sections/Home.jsx';
-import Speakers from "../sections/Speakers";
-
 
 export default function Home() {
   return (
     <div>
       <Crew />
 
-      <HeroSection />
-      <About />
-       <Speakers />
+    
   
     </div>
   );
