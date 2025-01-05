602b6cf85134a0ffb483dc091a0d64d1b648cc32
UTKARSHA-TEWARY <utkarsha7781@gmail.com>
2025-01-05 16:20:06 +0530

    Resolved merge conflicts in page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,15 @@
-
 import Crew from '../sections/Crew';
-
+import About from '../sections/About.jsx';
+import { HeroSection } from '../sections/Home.jsx';
+import Speakers from "../sections/Speakers";
 
 export default function Home() {
   return (
     <div>
+      <HeroSection />
       <Crew />
-
-    
-  
+      <About />
+      <Speakers />
     </div>
   );
 }
